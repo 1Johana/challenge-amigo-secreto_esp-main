1d1efd747b599b626dc0270738d1e2258da264ce
Johana Guerrero <yhullianaguerrero@gmail.com>
2025-02-04 20:17:29 -0500

    Agregando funcionalidad a los botones

--- app.js
+++ app.js
@@ -1,1 +1,60 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [];
+let amigoGanador;
+
+function agregarTexto(idElemento, texto) {
+    let idElementoHTML = document.getElementById(idElemento, texto);
+    idElementoHTML.innerHTML = texto;
+    return;
+}
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+    //Verificar que los nombres no se repitan en la lista
+    if(listaDeAmigos.includes(nombre)){
+        alert('El nombre ya existe');
+
+    //Verificar que  la caja de texto no esté vacía
+    } else if(nombre === ''){
+        alert('"Por favor, inserte un nombre."');
+    } else {
+        let nombre = document.getElementById('amigo').value;
+        listaDeAmigos.push(nombre);
+    }
+    limpiarInput();
+    //console.log(nombre);
+    mostrarNombres();
+}
+
+function sortearAmigo() {
+    if(listaDeAmigos.length === 0){
+        agregarTexto('listaAmigos', "No hay ningún nombre");
+        return;
+    } 
+
+    let indiceGanador = Math.floor(Math.random() * listaDeAmigos.length);
+    amigoGanador = listaDeAmigos[indiceGanador];
+    console.log(indiceGanador);
+    agregarTexto('listaAmigos', `${amigoGanador}`);    
+    //juegoInicial();
+}
+
+function juegoInicial() {
+    agregarAmigo('listaAmigos', ``);
+    listaDeAmigos = [];
+}
+
+function mostrarNombres() {
+    for (let i = 0; i < listaDeAmigos.length; i++){
+        agregarTexto('listaAmigos', `${listaDeAmigos}`);
+    }
+}
+
+//limpiar la caja de texto
+function limpiarInput() {
+    document.getElementById('amigo').value = '';
+}
+
+
+
+//console.log(nombreDeAmigos);
