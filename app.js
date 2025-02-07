d49264b58a6d46a54734e37d483c8099e481bb1d
Johana Guerrero <yhullianaguerrero@gmail.com>
2025-02-07 12:54:23 -0500

    Correcciones en la adición de nombres

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ function agregarTexto(idElemento, texto) {
 function agregarAmigo() {
     let nombre = document.getElementById('amigo').value;
     //Verificar que los nombres no se repitan en la lista
-    if(listaDeAmigos.includes(nombre)){
+    if (listaDeAmigos.includes(nombre)) {
         alert('El nombre ya existe');
 
-    //Verificar que  la caja de texto no esté vacía
-    } else if(nombre === ''){
+        //Verificar que  la caja de texto no esté vacía
+    } else if (nombre === '') {
         alert('"Por favor, inserte un nombre."');
     } else {
         let nombre = document.getElementById('amigo').value;
@@ -27,26 +27,38 @@ function agregarAmigo() {
 }
 
 function sortearAmigo() {
-    if(listaDeAmigos.length === 0){
+    //Verifica si hay elementos en el arreglo 
+    if (listaDeAmigos.length === 0) {
         agregarTexto('listaAmigos', "No hay ningún nombre");
         return;
     } 
-
     let indiceGanador = Math.floor(Math.random() * listaDeAmigos.length);
     amigoGanador = listaDeAmigos[indiceGanador];
     console.log(indiceGanador);
-    agregarTexto('listaAmigos', `${amigoGanador}`);    
+    agregarTexto('resultado', `El ganador del sorteo es: ${amigoGanador}`);
     //juegoInicial();
 }
 
+//Condiciones iniciales
 function juegoInicial() {
-    agregarAmigo('listaAmigos', ``);
     listaDeAmigos = [];
+    agregarTexto('resultado', '');
+    mostrarNombres();
 }
 
 function mostrarNombres() {
-    for (let i = 0; i < listaDeAmigos.length; i++){
-        agregarTexto('listaAmigos', `${listaDeAmigos}`);
+    const ul = document.getElementById("listaAmigos");
+
+    // Limpiar la lista antes de agregar nuevos nombres
+    ul.innerHTML = "";
+
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        //Crear el elemento "li"
+        const li = document.createElement("li");
+        //Agregar texto a "li"
+        li.textContent = listaDeAmigos[i]; 
+        // Agrega el "li" al "ul"
+        ul.appendChild(li); 
     }
 }
 
@@ -55,6 +67,7 @@ function limpiarInput() {
     document.getElementById('amigo').value = '';
 }
 
-
-
-//console.log(nombreDeAmigos);
+//Reinicia el sorteo
+function nuevoSorteo() {
+    juegoInicial();
+}
